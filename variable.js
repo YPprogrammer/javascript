120e7358af45fafeeb42d470e0a0295fb0a40e7b
YPprogrammer <paulyuvraj86@gmail.com>
2026-02-03 21:38:59 +0600

    first commit

---
+++ variable.js
@@ -0,0 +1,59 @@
+var age = (21);
+console.log(age);
+
+var number = (2005)
+number = (20202635)
+console.log(number);
+
+let car = ("bmw");
+car = "Parado";
+console.log(car);
+
+// const number = (2005);
+// console.log(number);
+
+var ispassed = false;
+console.log(ispassed);
+
+var passed = true;
+console.log(passed);
+
+
+var passed = false;
+console.log(typeof passed);
+
+var mangoPrice = 500;
+var orangePrice = 1000;
+var totalPrice = mangoPrice+orangePrice
+console.log(totalPrice);
+
+var price = 10;
+price +=30;
+console.log(price)
+
+var number = 50;
+number*=3
+console.log(number);
+
+var mangoPrice = 500;
+var orangePrice = 1000;
+var totalPrice = mangoPrice-orangePrice
+console.log(totalPrice);
+
+
+var number = 50;
+number/=3
+console.log( number.toFixed(4));
+
+const num = 50/0;
+console.log(num); 
+
+var age = 7;
+age %=2;
+console.log(age);
+
+var lamaPrice = 10;
+var bamaPrice = parseInt(5.3);
+var totalp = lamaPrice+bamaPrice
+console.log(totalp);
+
