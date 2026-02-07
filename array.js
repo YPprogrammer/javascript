554c0a8ead7f6d44a0a03ff9566670c47eb5a2f5
YPprogrammer <paulyuvraj86@gmail.com>
2026-02-07 22:17:50 +0600

    new js added

---
+++ array.js
@@ -0,0 +1,44 @@
+const numbers = [25,32,62,95,12,42,72,7,5];
+console.log(numbers)
+
+const fruits =["apple","banana","mango"];
+console.log(fruits)
+
+// mixed array
+const mixedArray =[21,"yuvraj",21.3,true];
+console.log(mixedArray);
+
+// value set
+const name =["apple","banana","mango"];
+name [0] = "sobeda";
+console.log(name);
+
+// index
+console.log(name[1])
+
+// length
+console.log(name.length)
+
+// push
+const ages = [12,24,56,89,23];
+ages.push(88);
+console.log(ages)
+
+// pop
+name.pop();
+console.log(name)
+
+// shift
+name.shift();
+console.log(name);
+
+// unshift
+name.unshift("kola");
+console.log(name)
+
+// number of index, index of()
+
+
+// problem, >> console.log(ages.indexof(23));
+
+console.log (Array.isArray(name))
